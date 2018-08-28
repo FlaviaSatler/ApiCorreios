632afb9c28ed731d14b35a33e4ae28c1dd12d957
Flavia <flaviasatler@hotmail.com>
2018-08-28 12:08:23 -0300

    Consulta completada

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#cep').blur(function(){
+        var cep = $('#cep').val() || '';
+        if(!cep){
+            return
+        }
+        var url = 'https://viacep.com.br/ws/' + cep +'/json';
+        $.getJSON(url, function(data){
+            if("error" in data){
+                return
+            }
+
+            $('#rua').text(data.logradouro);
+            $('#bairro').text(data.bairro);
+            $('#cidade').text(data.localidade);
+            $('#estado').text(data.uf);
+        });
+    });
+});
